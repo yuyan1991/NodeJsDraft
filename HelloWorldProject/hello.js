4e4fcf38f7dc02b8e6c5b1794e2b2c1ef60cd986
yuyan <yuyan1991@gmail.com>
2018-03-19 18:08:17 +0800

    Module for hello.js

--- HelloWorldProject/hello.js
+++ HelloWorldProject/hello.js
@@ -3,4 +3,18 @@
 var name = 'Node.js';
 var s = `Hello, ${name}!`;
 
-console.log(s);
+// console.log(s);
+
+var pre = 'Hello';
+
+function greet(name) {
+	console.log("greet: " + pre + ", " + name + "!");
+}
+
+function sayHi(name) {
+	console.log("sayHi: " + pre + ", " + name + "!");
+}
+module.exports = {
+	greet: greet,
+	sayHi: sayHi
+}
---
+++ HelloWorldProject/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var greet = require('./hello').greet;
+var sayHi = require('./hello').sayHi;
+
+var s = 'Node.js';
+
+greet(s);
+sayHi(s);
